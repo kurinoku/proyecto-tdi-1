60886fd9a92f2f2646c4de59c11a27a68cf1f4a3
Jean7721 <jsaavedray@ing.ucsc.cl>
2021-11-23 14:04:28 -0300

    Estilo a formularios

--- Mantenedores/Form_persona.php
+++ Mantenedores/Form_persona.php
@@ -7,26 +7,35 @@ require('conexion_p.php');
 <head>
     <meta charset="UTF-8">
     <title>Formulario persona</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
+<div class="container-fluid bg-success">
+    <div class="card-body bg-info">
+        <div class="mb-3" style="width: 230px;">
+            <div class="bg-warning m-1 ps-3 pb-2">
+                <h4>Formulario para añadir una persona</h4>
+                <form action="ingresar_persona.php" method="post">
+                    <label class="form-label d-block">Rut:</label>
+                    <input type="text" name="Rut_persona" placeholder="11111111-1"/>
+                    <label class="form-label d-block">Id municipalidad:</label>
+                    <input type="text" name="Id_municipalidad" placeholder="12345"/>
+                    <label class="form-label d-block">Nombre:</label>
+                    <input type="text" name="Nombre_persona" placeholder="Diego Zureña"/>
+                    <label class="form-label d-block">Numero:</label>
+                    <input type="text" name="Numero_persona" placeholder="912345678"/>
+                    <label class="form-label d-block">Correo:</label>
+                    <input type="text" name="Correo_persona" placeholder="Diego@ucsc.cl"/>
+                    <label class="form-label d-block">Clave:</label>
+                    <input type="password" name="Clave_persona" placeholder="**********"/>
+                    <button type="submit" class="d-block mt-2" style="width: 188px;">Guardar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>    
     
-    <form action="ingresar_persona.php" method="post">
-        <label>Rut:</label>
-        <input type="text" name="Rut_persona"/>
-        <label>Id municipalidad:</label>
-        <input type="text" name="Id_municipalidad"/>
-        <label>Nombre:</label>
-        <input type="text" name="Nombre_persona"/>
-        <label>Numero:</label>
-        <input type="text" name="Numero_persona"/>
-        <label>Correo:</label>
-        <input type="text" name="Correo_persona"/>
-        <label>Clave:</label>
-        <input type="text" name="Clave_persona"/>
-        
-
-        <button type="submit">Guardar</button>
-    </form>
 
     <br><br>
 
