8d7004c1a2a33e76d5b87988fff8b6aa5b390687
kurinoku <31811692+kurinoku@users.noreply.github.com>
2021-11-25 15:04:50 -0300

    Modificado formularios edit para ajustarse al nuevo estilo

--- Mantenedores/editar_departamento.php
+++ Mantenedores/editar_departamento.php
@@ -1,69 +1,57 @@
 <?php
-require('conexion_p.php');
+require "conexion_p.php";
+$codigo = $_GET["seleccionado"];
+$consulta = "SELECT * FROM departamento WHERE `Codigo_dep`='$codigo'";
+$resultado = mysqli_query($conexion, $consulta);
+$row = mysqli_fetch_assoc($resultado);
+$idMunicipalidad = $row["Id_municipalidad"];
+$rutAdministrador = $row["Rut_administrador"];
+$nombre = $row["Nombre_dep"];
+$encargado = $row["Encargado_departamento"];
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
-    <title>Editar departamento</title>
+    <title>Editar Departamento</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body>
-    
-    <form action="actualizar_departamento.php" method="post">
-        <label>Codigo</label><input type="text" name="Codigo_dep">
-        <label>Id Municipalidad</label><input type="text" name="Id_municipalidad">
-        <label>Rut Administrador</label><input type="text" name="Rut_administrador">
-        <label>Nombre</label><input type="text" name="Nombre_dep">
-        <label>Encargado</label><input type="text" name="Encargado_departamento">   
-        <button type="submit">Editar</button>
-    </form>
 
-    <br><br>
-
-    <table style="width:80%">
-        <tr>
-            <th>Codigo</th>
-            <th>Id municipalidad</th>
-            <th>Rut administrador</th>
-            <th>Nombre departamento</th>
-            <th>Encargado</th>
-        </tr>
-    
-
-    <?php
-        $consulta = "SELECT * FROM departamento";
-        $resultado = mysqli_query($conexion,$consulta);
-
-        while($row = mysqli_fetch_assoc($resultado)){
-            $Codigo = $row['Codigo_dep'];
-            $Id = $row['Id_municipalidad'];
-            $Rut = $row['Rut_administrador'];
-            $Nombre = $row['Nombre_dep'];
-            $Encargado = $row['Encargado_departamento'];
-
-
-            echo "<tr>";
-            echo "<td>".$Codigo."</td>";
-            echo "<td>".$Id."</td>";
-            echo "<td>".$Rut."</td>";
-            echo "<td>".$Nombre."</td>";
-            echo "<td>".$Encargado."</td>";
-            echo "</tr>";
-        }
-    ?>
-
-    </table>
-    <br>
-    <a href="javascript: history.go(-1)">Volver</a>
-    <script src="filtrar_form.js"></script>
-    <script>
-        let form = document.querySelector('form');
-        filtrarFormHook(
-            form, 
-            form.querySelector('input[type="text"]'),
-            () => alert('Codigo no ingresado')
-        );
-    </script>
+<body><?php require 'Navbar.html'; ?><div class="container">
+        <div class="row">
+            <div class="col-lg-6 mb-4">
+                <h4>Formulario para editar un departamento</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 mb-4">
+                <form action="ingresar_departamento.php" method="post"><label class="form-label d-block">Codigo Dep:</label>
+                    <input name="Codigo_dep" type="text" placeholder="" value="<?php
+                                                                                echo ($codigo);
+                                                                                ?>">
+                    <label class="form-label d-block">Id Municipalidad:</label>
+                    <input name="Id_municipalidad" type="text" placeholder="" value="<?php
+                                                                                        echo ($idMunicipalidad);
+                                                                                        ?>">
+                    <label class="form-label d-block">Rut Administrador:</label>
+                    <input name="Rut_administrador" type="text" placeholder="" value="<?php
+                                                                                        echo ($rutAdministrador);
+                                                                                        ?>">
+                    <label class="form-label d-block">Nombre Dep:</label>
+                    <input name="Nombre_dep" type="text" placeholder="" value="<?php
+                                                                                echo ($nombre);
+                                                                                ?>">
+                    <label class="form-label d-block">Encargado Departamento:</label>
+                    <input name="Encargado_departamento" type="text" placeholder="" value="<?php
+                                                                                            echo ($encargado);
+                                                                                            ?>">
+                    <button type="submit" class="d-block mt-2">Guardar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- Mantenedores/editar_persona.php
+++ Mantenedores/editar_persona.php
@@ -1,73 +1,62 @@
 <?php
-require('conexion_p.php');
+require "conexion_p.php";
+$rut = $_GET["seleccionado"];
+$consulta = "SELECT * FROM ciudadano WHERE `Rut_persona`='$rut'";
+$resultado = mysqli_query($conexion, $consulta);
+$row = mysqli_fetch_assoc($resultado);
+$idMunicipalidad = $row["Id_municipalidad"];
+$nombre = $row["Nombre_persona"];
+$numero = $row["Numero_persona"];
+$correo = $row["Correo_persona"];
+$clavePersona = $row["Clave_persona"];
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
-    <title>Editar ciudadano</title>
+    <title>Editar Persona</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body>
-    
-    <form action="actualizar_persona.php" method="post">
-        <label>Rut</label><input type="text" name="Rut_persona">
-        <label>Id Municipalidad</label><input type="text" name="Id_municipalidad">
-        <label>Nombre</label><input type="text" name="Nombre_persona">
-        <label>Numero</label><input type="text" name="Numero_persona">
-        <label>Correo</label><input type="text" name="Correo_persona">
-        <label>Clave Persona</label><input type="text" name="Clave_persona">
 
-        <button type="submit">Editar</button>
-    </form>
-
-    <br><br>
-
-    <table style="width:80%">
-        <tr>
-            <th>Rut</th>
-            <th>Id municipalidad</th>
-            <th>Nombre</th>
-            <th>Numero</th>
-            <th>Correo</th>
-            <th>Clave</th>
-        </tr>
-    
-
-    <?php
-        $consulta = "SELECT * FROM ciudadano";
-        $resultado = mysqli_query($conexion,$consulta);
-
-        while($row = mysqli_fetch_assoc($resultado)){
-            $rut = $row['Rut_persona'];
-            $municipalidad = $row['Id_municipalidad'];
-            $nombre = $row['Nombre_persona'];
-            $numero = $row['Numero_persona'];
-            $correo = $row['Correo_persona'];
-            $clave = $row['Clave_persona'];
-            
-            echo "<tr>";
-            echo "<td>".$rut."</td>";
-            echo "<td>".$municipalidad."</td>";
-            echo "<td>".$nombre."</td>";
-            echo "<td>".$numero."</td>";
-            echo "<td>".$correo."</td>";
-            echo "<td>".$clave."</td>";
-            echo "</tr>";
-        }
-    ?>
-
-    </table>
-    <br>
-    <a href="javascript: history.go(-1)">Volver</a>
-    <script src="filtrar_form.js"></script>
-    <script>
-        let form = document.querySelector('form');
-        filtrarFormHook(
-            form, 
-            form.querySelector('input[type="text"]'),
-            () => alert('Rut no ingresado')
-        );
-    </script>
+<body><?php require 'Navbar.html'; ?><div class="container">
+        <div class="row">
+            <div class="col-lg-6 mb-4">
+                <h4>Formulario para editar un persona</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 mb-4">
+                <form action="ingresar_persona.php" method="post"><label class="form-label d-block">Rut Persona:</label>
+                    <input name="Rut_persona" type="text" placeholder="" value="<?php
+                                                                                echo ($rut);
+                                                                                ?>">
+                    <label class="form-label d-block">Id Municipalidad:</label>
+                    <input name="Id_municipalidad" type="text" placeholder="" value="<?php
+                                                                                        echo ($idMunicipalidad);
+                                                                                        ?>">
+                    <label class="form-label d-block">Nombre Persona:</label>
+                    <input name="Nombre_persona" type="text" placeholder="" value="<?php
+                                                                                    echo ($nombre);
+                                                                                    ?>">
+                    <label class="form-label d-block">Numero Persona:</label>
+                    <input name="Numero_persona" type="text" placeholder="" value="<?php
+                                                                                    echo ($numero);
+                                                                                    ?>">
+                    <label class="form-label d-block">Correo Persona:</label>
+                    <input name="Correo_persona" type="text" placeholder="" value="<?php
+                                                                                    echo ($correo);
+                                                                                    ?>">
+                    <label class="form-label d-block">Clave Persona:</label>
+                    <input name="Clave_persona" type="password" placeholder="" value="<?php
+                                                                                        echo ($clavePersona);
+                                                                                        ?>">
+                    <button type="submit" class="d-block mt-2">Guardar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
+
 </html>
