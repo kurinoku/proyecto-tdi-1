44589bf7c0ce7fa4e7c2fd985d77aa7be5b06f5b
Jean7721 <jsaavedray@ing.ucsc.cl>
2021-11-29 20:05:21 -0300

    Correción Scroll y Footer
    Se realiza correción del scroll horizontal, y que el footer quede en la parte inferior de la página.

--- Mantenedores/Form_administrador.php
+++ Mantenedores/Form_administrador.php
@@ -16,82 +16,84 @@ require('conexion_p.php');
     <!-- Diseños -->
 </head>
 
-<body>
-    <!-- Barra de navegación -->
-    <?php
-    require('Navbar.html');
-    ?>
-    <!-- Contenedor del Formulario y la Tabla -->
-    <div class="row flex-lg-row">
-        <!-- Formulario -->
-        <div class="col-lg-6 col-md-12">
-            <form action="ingresar_administrador.php" method="post">
-                <fieldset>
-                    <legend class="text-center pt-3">Formulario para añadir Administrador</legend>
-                    <div class="form-group row">
-                        <div class="form-group">
-                            <label>RUT</label>
-                            <input type="text" name="Rut_administrador" class="form-control" placeholder="12345678 (Sin guión)" required>
-                        </div>
-                        <div class="form-group mt-2">
-                            <label>Nombre</label>
-                            <input type="text" name="Nombre_administrador" class="form-control" placeholder="Nombre completo" required>
-                        </div>
-                        <div class="form-group mt-2">
-                            <label>Número de celular</label>
-                            <input type="text" name="Numero_administrador" class="form-control" placeholder="991738264" required>
-                            <small class="form-text text-muted">Utiliza un número de celular que ocupes de manera recurrente.</small>
-                        </div>
-                        <div class="form-group mt-2">
-                            <label>E-mail</label>
-                            <input type="email" name="Correo_trabajo" class="form-control" placeholder="alguien@example.com" required>
-                            <small class="form-text text-muted">Utiliza un correo principal y no lo compartas con nadie.</small>
-                        </div>
-                        <div class="form-group mt-2">
-                            <label>Contraseña</label>
-                            <input type="password" name="Clave_ingreso" class="form-control" placeholder="********" required>
-                            <small class="form-text text-muted">Utiliza contraseña segura entre 8 y 16 caracteres.</small>
-                        </div>
-                </fieldset>
-                <div class="text-center pb-1">
-                    <button type="submit" class="btn btn-primary mt-2">Registrar Administrador</button>
-                </div>
-            </form>
-        </div>
-        <!-- Tabla -->
-        <div class="col-lg-6 col-md-12 ps-1">
-            <legend class="text-center pt-3">Registro de los administradores</legend>
-            <table class="table table-striped table-hover">
-                <thead class="bg-dark text-light">
-                    <tr>
-                        <th>RUT</th>
-                        <th>Nombre</th>
-                        <th>N° Celular</th>
-                        <th>E-mail</th>
-                        <th>Opciones</th>
-                    </tr>
-                </thead>
-                <?php
-                $consulta = "SELECT * FROM administrador";
-                $resultado = mysqli_query($conexion, $consulta);
+<body class="d-flex flex-column min-vh-100">
+    <div class="container-fluid">
+        <!-- Barra de navegación -->
+        <?php
+        require('Navbar.html');
+        ?>
+        <!-- Contenedor del Formulario y la Tabla -->
+        <div class="row flex-lg-row">
+            <!-- Formulario -->
+            <div class="col-lg-6 col-md-12">
+                <form action="ingresar_administrador.php" method="post">
+                    <fieldset>
+                        <legend class="text-center pt-3">Formulario para añadir Administrador</legend>
+                        <div class="form-group row">
+                            <div class="form-group">
+                                <label>RUT</label>
+                                <input type="text" name="Rut_administrador" class="form-control" placeholder="12345678 (Sin guión)" required>
+                            </div>
+                            <div class="form-group mt-2">
+                                <label>Nombre</label>
+                                <input type="text" name="Nombre_administrador" class="form-control" placeholder="Nombre completo" required>
+                            </div>
+                            <div class="form-group mt-2">
+                                <label>Número de celular</label>
+                                <input type="text" name="Numero_administrador" class="form-control" placeholder="991738264" required>
+                                <small class="form-text text-muted">Utiliza un número de celular que ocupes de manera recurrente.</small>
+                            </div>
+                            <div class="form-group mt-2">
+                                <label>E-mail</label>
+                                <input type="email" name="Correo_trabajo" class="form-control" placeholder="alguien@example.com" required>
+                                <small class="form-text text-muted">Utiliza un correo principal y no lo compartas con nadie.</small>
+                            </div>
+                            <div class="form-group mt-2">
+                                <label>Contraseña</label>
+                                <input type="password" name="Clave_ingreso" class="form-control" placeholder="********" required>
+                                <small class="form-text text-muted">Utiliza contraseña segura entre 8 y 16 caracteres.</small>
+                            </div>
+                    </fieldset>
+                    <div class="text-center pb-1">
+                        <button type="submit" class="btn btn-primary mt-2">Registrar Administrador</button>
+                    </div>
+                </form>
+            </div>
+            <!-- Tabla -->
+            <div class="col-lg-6 col-md-12 ps-1">
+                <legend class="text-center pt-3">Registro de los administradores</legend>
+                <table class="table table-striped table-hover">
+                    <thead class="bg-dark text-light">
+                        <tr>
+                            <th>RUT</th>
+                            <th>Nombre</th>
+                            <th>N° Celular</th>
+                            <th>E-mail</th>
+                            <th>Opciones</th>
+                        </tr>
+                    </thead>
+                    <?php
+                    $consulta = "SELECT * FROM administrador";
+                    $resultado = mysqli_query($conexion, $consulta);
 
-                while ($row = mysqli_fetch_assoc($resultado)) {
-                    $Rut = $row['Rut_administrador'];
-                    $Nombre = $row['Nombre_administrador'];
-                    $Numero = $row['Numero_administrador'];
-                    $Correo = $row['Correo_trabajo'];
-                    $Clave = $row['Clave_ingreso'];
+                    while ($row = mysqli_fetch_assoc($resultado)) {
+                        $Rut = $row['Rut_administrador'];
+                        $Nombre = $row['Nombre_administrador'];
+                        $Numero = $row['Numero_administrador'];
+                        $Correo = $row['Correo_trabajo'];
+                        $Clave = $row['Clave_ingreso'];
 
-                    echo "<tr>";
-                    echo "<td>" . $Rut . "</td>";
-                    echo "<td>" . $Nombre . "</td>";
-                    echo "<td>" . $Numero . "</td>";
-                    echo "<td>" . $Correo . "</td>";
-                    echo "<td><a href='eliminar_administrador.php?seleccionado=" . $Rut . "'>Eliminar</a> <a href='editar_administrador.php?seleccionado=" . $Rut . "'>Editar</a></td>";
-                    echo "</tr>";
-                }
-                ?>
-            </table>
+                        echo "<tr>";
+                        echo "<td>" . $Rut . "</td>";
+                        echo "<td>" . $Nombre . "</td>";
+                        echo "<td>" . $Numero . "</td>";
+                        echo "<td>" . $Correo . "</td>";
+                        echo "<td><a href='eliminar_administrador.php?seleccionado=" . $Rut . "'>Eliminar</a> <a href='editar_administrador.php?seleccionado=" . $Rut . "'>Editar</a></td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </table>
+            </div>
         </div>
     </div>
     <!-- Footer -->
--- Mantenedores/Form_municipalidad.php
+++ Mantenedores/Form_municipalidad.php
@@ -16,72 +16,74 @@ require('conexion_p.php');
     <!-- Diseños -->
 </head>
 
-<body>
-    <!-- Barra de navegación -->
-    <?php
-    require('Navbar.html');
-    ?>
-    <!-- Contenedor del Formulario y la Tabla -->
-    <div class="row flex-lg-row">
-        <!-- Formulario -->
-        <div class="col-lg-6 col-md-12">
-            <form action="ingresar_municipalidad.php" method="post">
-                <fieldset>
-                    <legend class="text-center pt-3">Formulario para añadir Municipalidad</legend>
-                    <div class="form-group row">
-                        <div class="form-group">
-                            <label>Id municipalidad</label>
-                            <input type="text" name="Id_municipalidad" class="form-control" placeholder="12345" required>
-                        </div>
-                        <div class="form-group mt-2">
-                            <label>Nombre municipalidad</label>
-                            <input type="text" name="Nombre_municipalidad" class="form-control" placeholder="Municipalidad de Concepción" required>
-                        </div>
-                        <div class="form-group mt-2">
-                            <label>Direccion municipalidad</label>
-                            <input type="text" name="Direccion_municipalidad" class="form-control" placeholder="Pasaje Barros Arana 332" required>
-                        </div>
-                        <div class="form-group mt-2">
-                            <label>Numero municipalidad:</label>
-                            <input type="text" name="Numero_municipalidad" class="form-control" placeholder="978451232" required>
-                        </div>
-                </fieldset>
-                <div class="text-center pb-1">
-                    <button type="submit" class="btn btn-primary mt-2">Registrar Municipalidad</button>
-                </div>
-            </form>
-        </div>
-        <!-- Tabla -->
-        <div class="col-lg-6 col-md-12 ps-1">
-            <legend class="text-center pt-3">Registro de la Municipalidad</legend>
-            <table class="table table-striped table-hover">
-                <thead class="bg-dark text-light">
-                    <tr>
-                        <th>Id municipalidad</th>
-                        <th>Nombre municipalidad</th>
-                        <th>Direccion municipalidad</th>
-                        <th>Numero municipalidad</th>
-                        <th>Opciones</th>
-                    </tr>
-                </thead>
-                <?php
-                $consulta = "SELECT * FROM municipalidad";
-                $resultado = mysqli_query($conexion, $consulta);
-                while ($row = mysqli_fetch_assoc($resultado)) {
-                    $id = $row['Id_municipalidad'];
-                    $nombre = $row['Nombre_municipalidad'];
-                    $direccion = $row['Direccion_municipalidad'];
-                    $numero = $row['Numero_municipalidad'];
-                    echo "<tr>";
-                    echo "<td>" . $id . "</td>";
-                    echo "<td>" . $nombre . "</td>";
-                    echo "<td>" . $direccion . "</td>";
-                    echo "<td>" . $numero . "</td>";
-                    echo "<td> <a href='editar_municipalidad.php?seleccionado=" . $id . "'>Editar</a></td>";
-                    echo "</tr>";
-                }
-                ?>
-            </table>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container-fluid">
+        <!-- Barra de navegación -->
+        <?php
+        require('Navbar.html');
+        ?>
+        <!-- Contenedor del Formulario y la Tabla -->
+        <div class="row flex-lg-row">
+            <!-- Formulario -->
+            <div class="col-lg-6 col-md-12">
+                <form action="ingresar_municipalidad.php" method="post">
+                    <fieldset>
+                        <legend class="text-center pt-3">Formulario para añadir Municipalidad</legend>
+                        <div class="form-group row">
+                            <div class="form-group">
+                                <label>Id municipalidad</label>
+                                <input type="text" name="Id_municipalidad" class="form-control" placeholder="12345" required>
+                            </div>
+                            <div class="form-group mt-2">
+                                <label>Nombre municipalidad</label>
+                                <input type="text" name="Nombre_municipalidad" class="form-control" placeholder="Municipalidad de Concepción" required>
+                            </div>
+                            <div class="form-group mt-2">
+                                <label>Direccion municipalidad</label>
+                                <input type="text" name="Direccion_municipalidad" class="form-control" placeholder="Pasaje Barros Arana 332" required>
+                            </div>
+                            <div class="form-group mt-2">
+                                <label>Numero municipalidad:</label>
+                                <input type="text" name="Numero_municipalidad" class="form-control" placeholder="978451232" required>
+                            </div>
+                    </fieldset>
+                    <div class="text-center pb-1">
+                        <button type="submit" class="btn btn-primary mt-2">Registrar Municipalidad</button>
+                    </div>
+                </form>
+            </div>
+            <!-- Tabla -->
+            <div class="col-lg-6 col-md-12 ps-1">
+                <legend class="text-center pt-3">Registro de la Municipalidad</legend>
+                <table class="table table-striped table-hover">
+                    <thead class="bg-dark text-light">
+                        <tr>
+                            <th>Id municipalidad</th>
+                            <th>Nombre municipalidad</th>
+                            <th>Direccion municipalidad</th>
+                            <th>Numero municipalidad</th>
+                            <th>Opciones</th>
+                        </tr>
+                    </thead>
+                    <?php
+                    $consulta = "SELECT * FROM municipalidad";
+                    $resultado = mysqli_query($conexion, $consulta);
+                    while ($row = mysqli_fetch_assoc($resultado)) {
+                        $id = $row['Id_municipalidad'];
+                        $nombre = $row['Nombre_municipalidad'];
+                        $direccion = $row['Direccion_municipalidad'];
+                        $numero = $row['Numero_municipalidad'];
+                        echo "<tr>";
+                        echo "<td>" . $id . "</td>";
+                        echo "<td>" . $nombre . "</td>";
+                        echo "<td>" . $direccion . "</td>";
+                        echo "<td>" . $numero . "</td>";
+                        echo "<td> <a href='editar_municipalidad.php?seleccionado=" . $id . "'>Editar</a></td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </table>
+            </div>
         </div>
     </div>
     <!-- Footer -->
