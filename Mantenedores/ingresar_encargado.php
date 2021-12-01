b240512e842d12ccb2b8bc0bca4fbfb1afd61f20
kurinoku <31811692+kurinoku@users.noreply.github.com>
2021-11-30 22:30:58 -0300

    fix ingresar_encargado.php: usar md5 para clave

--- Mantenedores/ingresar_encargado.php
+++ Mantenedores/ingresar_encargado.php
@@ -4,7 +4,7 @@ $Rut=$_POST['Rut_encargado'];
 $Nombre=$_POST['Nombre_encargado'];
 $Numero=$_POST['Numero_encargado'];
 $Correo=$_POST['Correo_encargado'];
-$Clave=$_POST['Clave_encargado'];
+$Clave=md5($_POST['Clave_encargado']);
 $sql = "INSERT INTO encargado VALUES ('$Rut', '$Nombre', '$Numero', '$Correo', '$Clave')";
 $result = mysqli_query($conexion, $sql);
 header('Location: Form_encargado.php');
