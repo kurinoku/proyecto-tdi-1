f051c00793851bd37350b574f87656e191cc1400
GustavoSepul <gsepulvedao@ing.ucsc.cl>
2021-12-01 00:46:55 -0300

    Arreglo Navbar
    arreglo en el nombre de la navbar en aditar administrador, departamento, encargado, municipalidad, persona y form encargado. Ademas arrelgo en el order de los valores al ingresar municipalidad

--- Mantenedores/editar_persona.php
+++ Mantenedores/editar_persona.php
@@ -19,7 +19,7 @@ $clavePersona = $row["Clave_persona"];
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
-<body><?php require 'Navbar.html'; ?><div class="container">
+<body><?php require 'Navbar_administrador.html'; ?><div class="container">
         <div class="row">
             <div class="col-lg-6 mb-4">
                 <h4>Formulario para editar un persona</h4>
--- Mantenedores/ingresar_municipalidad.php
+++ Mantenedores/ingresar_municipalidad.php
@@ -5,7 +5,7 @@ $nombre=$_POST['Nombre_municipalidad'];
 $Rut = $_POST['Rut_administrador'];
 $direccion=$_POST['Direccion_municipalidad'];
 $numero=$_POST['Numero_municipalidad'];
-$sql = "INSERT INTO municipalidad VALUES ('$id','$Rut', '$nombre',  '$direccion', '$numero')";
+$sql = "INSERT INTO municipalidad VALUES ('$id', '$nombre','$Rut', '$direccion', '$numero')";
 $result = mysqli_query($conexion, $sql);
 header('Location: Form_municipalidad.php');
 ?>
