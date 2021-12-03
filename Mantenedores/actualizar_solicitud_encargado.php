4df8e35b112bad62c22e8fa71b9e18e1e74d76e8
kurinoku <31811692+kurinoku@users.noreply.github.com>
2021-12-02 23:28:06 -0300

    Integrado enviar correo al actualizar el estado de solicitud

--- Mantenedores/actualizar_solicitud_encargado.php
+++ Mantenedores/actualizar_solicitud_encargado.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'conexion_p.php';
+require_once 'solicitud/enviar_correo.php';
 
 header('content-type: application/json;charset=UTF-8');
 
@@ -18,6 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $nuevoEstado = $estado;
         if ($i + 1 < count($estados)) {
             $nuevoEstado = $estados[$i + 1];
+            enviarCorreo($codigo);
         }
 
         $sql = "UPDATE solicitud SET Estado_solicitud = '$nuevoEstado' WHERE Codigo_solicitud=$codigo";
