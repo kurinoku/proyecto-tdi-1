f739e66d007fea076ffd97f5908db7062530c777
kurinoku <31811692+kurinoku@users.noreply.github.com>
2021-12-16 00:08:41 -0300

    Reestructurazión para que el navbar de index sea reutilizado

--- Mantenedores/mapa.php
+++ Mantenedores/mapa.php
@@ -17,104 +17,10 @@ require_once "_init.php";
 
 <body class="d-flex flex-column min-vh-100">
     <div class="container-fluid">
-        <!-- Barra de navegación primaria-->
-        <nav class="row navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand ms-5" href=<?php echoRutaComillas("index.php"); ?>>Municipalidad de Concepción</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#TipoLogin">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div id="TipoLogin" class="collapse navbar-collapse">
-                        <button type="button" class="btn btn-secondary bg-dark ms-auto" onclick=<?php echo "\"location.href='" . buildRuta("login_persona.php") . "'\""; ?>>
-                            <?php session_start();if (isset($_SESSION['tipo'])){
-                                echo "Ir a cuenta";
-                                }else{
-                                    echo "Iniciar Sesión";
-                                }
-                            ?>
-                        </button>
-                </div>
-            </div>
-        </nav>
-        <!-- Barra de navegación secundaria -->
-        <nav class="row navbar navbar-expand-lg navbar-dark bg-secondary">
-            <div class="container">
-                <div class="container-fluid">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#Seccion"
-                        aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="Seccion">
-                        <ul class="navbar-nav me-auto mx-auto">
-                            <div class="col">
-                                <!-- Botón de Inicio -->
-                                <li class="nav-item">
-                                    <a class="nav-link active" href="#">Inicio</a>
-                                </li>
-                            </div>
-                            <div class="col">
-                                <!-- Botón de despliegue sección de Municipalidad -->
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link active dropdown-toggle ms-5" data-bs-toggle="dropdown" href="#"
-                                        role="button" aria-haspopup="true" aria-expanded="false">Municipalidad</a>
-                                    <div class="dropdown-menu">
-                                        <a class="dropdown-item" href="#">Direcciones Municipales</a>
-                                        <a class="dropdown-item" href="#">Concejo Municipal</a>
-                                        <a class="dropdown-item" href="#">Juzgados de Policía Local</a>
-                                        <a class="dropdown-item" href="#">Misión y Visión</a>
-                                        <a class="dropdown-item" href="#">Palabras del Alcalde</a>
-                                        <a class="dropdown-item" href="#">Himno</a>
-                                        <a class="dropdown-item" href="#">Participación Ciudadana</a>
-                                        <a class="dropdown-item" href="#">Plan Regulador</a>
-                                    </div>
-                                </li>
-                            </div>
-                            <div class="col">
-                                <!-- Botón de despliegue sección de Tramites -->
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link active dropdown-toggle ms-5" data-bs-toggle="dropdown" href="#"
-                                        role="button" aria-haspopup="true" aria-expanded="false">Trámites</a>
-                                    <div class="dropdown-menu">
-                                        <a class="dropdown-item" href="#">En Linea</a>
-                                        <a class="dropdown-item" href="#">Oficina de Partes</a>
-                                        <a class="dropdown-item" href="#">Trámites Jurídicos</a>
-                                        <a class="dropdown-item" href="#">Consulta de Documentos electrónicos</a>
-                                        <a class="dropdown-item" href="#">Actos y resoluciones con efectos sobre
-                                            terceras personas</a>
-                                        <a class="dropdown-item" href="#">Formularios</a>
-                                        <a class="dropdown-item" href="#">Otros Servicios</a>
-                                    </div>
-                                </li>
-                            </div>
-                            <div class="col">
-                                <!-- Botón de Servicios -->
-                                <li class="nav-item">
-                                    <a class="nav-link active ms-5" href="#">Servicios</a>
-                                </li>
-                            </div>
-                            <div class="col">
-                                <!-- Botón de Fotos -->
-                                <li class="nav-item">
-                                    <a class="nav-link active ms-5" href="#">Fotos</a>
-                                </li>
-                            </div>
-                            <div class="col">
-                                <!-- Botón de Noticias -->
-                                <li class="nav-item">
-                                    <a class="nav-link active ms-5" href="#">Noticias</a>
-                                </li>
-                            </div>
-                            <div class="col">
-                                <!-- Botón de Ubicación -->
-                                <li class="nav-item">
-                                    <a class="nav-link active ms-5" href=<? echoRutaComillas("Mantenedores/mapa.php") ?>>Ubicación</a>
-                                </li>
-                            </div>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </nav>
+        
+        <?php require __DIR__ . "/../navbar_index_1.php" ?>
+        <?php require __DIR__ . "/../navbar_index_2.php" ?>
+        
         <div class="container-fluid">
             <div class="row mt-3">
                 <h2 class="text-black text-center">Municipalidad de Concepción</h2>
